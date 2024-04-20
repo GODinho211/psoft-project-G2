87f23dc9ac79b64a17dee20a1827b5f7f9bc8e1c
henrique222 <1200968@isep.ipp.pt>
2024-04-20 15:28:51 +0100

    *unfinished* updated author files

--- src/main/java/com/example/projetopsoft2024/Controllers/AuthorController.java
+++ src/main/java/com/example/projetopsoft2024/Controllers/AuthorController.java
@@ -5,9 +5,12 @@ import com.example.projetopsoft2024.Service.AuthorService;
 import com.example.projetopsoft2024.models.Author;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/author")
@@ -28,6 +31,38 @@ public class AuthorController {
     @DeleteMapping("/{authorId}")
     public String deleteAuthor(@PathVariable long authorId){
         return authorService.deleteAuthor(authorId);
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<?> getAuthorByName(@PathVariable String name) {
+        List<Author> authors = authorService.findByName(name);
+        if (!authors.isEmpty()) {
+            return ResponseEntity.ok(authors);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No authors found with name " + name);
+        }
+    }
+    @GetMapping("/id/{id}")
+    public ResponseEntity<?> getAuthorById(@PathVariable Long id) {
+        Optional<Author> author = authorService.getAuthorById(id);
+        if (author.isPresent()) {
+            return ResponseEntity.ok(author.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Author with id " + id + " not found");
+        }
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateAuthor(@PathVariable Long id, @RequestBody Author updatedAuthor) {
+        Optional<Author> optionalAuthor = authorService.getAuthorById(id);
+        if (optionalAuthor.isPresent()) {
+            Author existingAuthor = optionalAuthor.get();
+            existingAuthor.updateName(updatedAuthor.getName());
+            existingAuthor.setBio(updatedAuthor.getBio());
+            authorService.updateAuthor(id, existingAuthor);
+            return ResponseEntity.ok("Author updated!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Author not found");
+        }
     }
 
 
