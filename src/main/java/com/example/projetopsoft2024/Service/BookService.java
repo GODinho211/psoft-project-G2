ecc3c7fd3d48af3fc84dece2871978447894d6a3
João Ribas <joao.ribas.2014@gmail.com>
2024-04-27 00:20:24 +0100

    get book by gender

--- src/main/java/com/example/projetopsoft2024/Service/BookService.java
+++ src/main/java/com/example/projetopsoft2024/Service/BookService.java
@@ -1,9 +1,14 @@
 package com.example.projetopsoft2024.Service;
 
 import com.example.projetopsoft2024.Repositories.BookRepository;
+import com.example.projetopsoft2024.Repositories.GenderRepository;
 import com.example.projetopsoft2024.models.Book;
+import com.example.projetopsoft2024.models.Gender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +19,10 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private GenderRepository GenderRepository;
+
+    @Transactional
     public List<Book> getAllBooks() {
        return bookRepository.findAll();
     }
@@ -25,6 +34,13 @@ public class BookService {
     public String deleteBook(long bookId) {
         bookRepository.deleteById(bookId);
         return "Book deleted";
+    }
+
+    @Transactional
+    public List<Book> getBooksByGender(Long genderId) {
+        Gender gender = GenderRepository.findById(genderId)
+                .orElseThrow(() -> new RuntimeException("Gender not found with id " + genderId));
+        return bookRepository.findByGender(gender);
     }
 }
 
