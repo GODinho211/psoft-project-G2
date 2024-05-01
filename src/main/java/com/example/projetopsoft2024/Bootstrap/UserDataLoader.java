4c5c9209adb3272f2d79ba28b62547b959542b30
1200908 <1200908@isep.ipp.pt>
2024-05-02 00:10:42 +0100

    minor update bootstrap

--- src/main/java/com/example/projetopsoft2024/Bootstrap/UserDataLoader.java
+++ src/main/java/com/example/projetopsoft2024/Bootstrap/UserDataLoader.java
@@ -22,16 +22,16 @@ public class UserDataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Inicializar usuários
+
         List<User> users = Arrays.asList(
                 new User("Alice", "alice@example.com", new Date(), 123456789L, 123456L, "sim"),
                 new User("Bob", "bob@example.com", new Date(), 987654321L, 654321L, "sim"),
                 new User("Charlie", "charlie@example.com", new Date(), 555555555L, 333333L, "sim"),
                 new User("Diana", "diana@example.com", new Date(), 111111111L, 999999L, "sim")
-                // Adicione mais usuários conforme necessário
+
         );
 
-        // Salvar os usuários no banco de dados
+
         userRepository.saveAll(users);
     }
 }
