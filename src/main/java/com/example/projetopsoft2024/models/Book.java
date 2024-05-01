2fc2e3ea85e31929afc16dabdae7404899881684
João Ribas <joao.ribas.2014@gmail.com>
2024-05-02 00:06:49 +0100

    bug fix

--- src/main/java/com/example/projetopsoft2024/models/Book.java
+++ src/main/java/com/example/projetopsoft2024/models/Book.java
@@ -36,7 +36,7 @@ public class Book {
     private List<Gender> gender= new ArrayList<>();
 
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
     @JoinTable(
             name = "book_gender",
             joinColumns = @JoinColumn(name = "book_id"),
