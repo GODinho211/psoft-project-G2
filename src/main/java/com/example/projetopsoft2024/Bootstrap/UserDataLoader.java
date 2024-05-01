8d9f993c1077e50e433076ef8cd4774e8cfa69b8
1200908 <1200908@isep.ipp.pt>
2024-05-02 00:07:50 +0100

    bootstrap user added

---
+++ src/main/java/com/example/projetopsoft2024/Bootstrap/UserDataLoader.java
@@ -0,0 +1,37 @@
+package com.example.projetopsoft2024.Bootstrap;
+
+;
+import com.example.projetopsoft2024.Repositories.GenderRepository;
+import com.example.projetopsoft2024.Repositories.UserRepository;
+import com.example.projetopsoft2024.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDataLoader implements CommandLineRunner {
+    private final UserRepository userRepository;
+    @Autowired
+    public UserDataLoader(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Inicializar usuários
+        List<User> users = Arrays.asList(
+                new User("Alice", "alice@example.com", new Date(), 123456789L, 123456L, "sim"),
+                new User("Bob", "bob@example.com", new Date(), 987654321L, 654321L, "sim"),
+                new User("Charlie", "charlie@example.com", new Date(), 555555555L, 333333L, "sim"),
+                new User("Diana", "diana@example.com", new Date(), 111111111L, 999999L, "sim")
+                // Adicione mais usuários conforme necessário
+        );
+
+        // Salvar os usuários no banco de dados
+        userRepository.saveAll(users);
+    }
+}
