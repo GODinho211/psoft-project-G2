acbee246739a3ec21e43da32367490d91e1b1f00
1200908 <1200908@isep.ipp.pt>
2024-06-06 17:43:15 +0100

    security update for anonym methods

--- src/main/java/com/example/projetopsoft2024/Api/SecurityConfigurations.java
+++ src/main/java/com/example/projetopsoft2024/Api/SecurityConfigurations.java
@@ -39,8 +39,8 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.GET,"/api/users/readernumber/{readernumber}").hasRole("LIBRARIAN")
                         .requestMatchers(HttpMethod.GET,"/api/users/name/{name}").hasRole("LIBRARIAN")
                         .requestMatchers(HttpMethod.GET,"/api/users/books/{readernumber}").hasRole("READER")
-                        .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
-
+                        .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/users/create").permitAll()
 
                         .requestMatchers(HttpMethod.GET, "/public/**").permitAll()
                         .requestMatchers("/images/**").permitAll()  // Allow access to 'images' directory
